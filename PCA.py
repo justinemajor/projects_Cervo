524dd3f0329b52508f87702cadd99e674238603d
Justine Major <jumaj7@ulaval.ca>
2021-05-28 14:53:07 -0400

    pas encore fonctionnelle, mais méthode pca commencée

--- PCA.py
+++ PCA.py
@@ -3,6 +3,8 @@ from tkinter.filedialog import askopenfile
 import csv
 import os
 import fnmatch
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 def listNameOfFiles(directory: str, extension="txt") -> list:
     foundFiles = []
@@ -17,8 +19,8 @@ def getFilePaths(directory: str, fileNames: list) -> list:
         filesWithFullPath.append(directory+"/"+fileName)
     return filesWithFullPath
 
-path = "/Users/justinemajor/Documents/gph.doc/stage1/documents/spectres"
-donnees_tot_x, donnees_tot_y = [], {}
+path = "/Users/justinemajor/Documents/gph.doc/stage1/documents/spectres/10"
+donnees_tot_x, ordo, donnees_tot_y = [], [], {}
 nb = len(listNameOfFiles(path))
 
 for nom in listNameOfFiles(path):
@@ -35,6 +37,13 @@ for nom in listNameOfFiles(path):
         y.append(float(elem[1]))
 
     donnees_tot_x = x
+    ordo.append(y)
     donnees_tot_y[nom] = y
 
-print(donnees_tot_y)
+ordo = StandardScaler().fit_transform(ordo)
+pca = PCA(n_components=4)
+principalComponents = pca.fit_transform(ordo)
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2', 'principal component 3', 'principal component 4'])
+print(principalDf)
+print(listNameOfFiles(path).index("0100_10.txt"))
+print(listNameOfFiles(path)[4:6])
