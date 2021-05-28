b09ecd6f084bc2001b81f726d6ff5c6d48a29b24
Justine Major <jumaj7@ulaval.ca>
2021-05-28 11:30:53 -0400

    code fonctionnel pour lire les fichiers

--- PCA.py
+++ PCA.py
@@ -1,8 +1,42 @@
 import pandas as pd
 from tkinter.filedialog import askopenfile
 import csv
+import os
+import fnmatch
 
-path = "/Users/justinemajor/Documents/gph.doc/stage1/documents/spectres/"
+def listNameOfFiles(directory: str, extension="txt") -> list:
+    foundFiles = []
+    for file in os.listdir(directory):
+        if fnmatch.fnmatch(file, f'*.{extension}'):
+            foundFiles.append(file)
+    return foundFiles
 
-données=pd.read_csv(path + "0001_01.txt", sep = r"\n")[14:]
-print(données)
+def getFilePaths(directory: str, fileNames: list) -> list:
+    filesWithFullPath = []
+    for fileName in fileNames:
+        filesWithFullPath.append(directory+"/"+fileName)
+    return filesWithFullPath
+
+path = "/Users/justinemajor/Documents/gph.doc/stage1/documents/spectres"
+donnees_tot_x = []
+donnees_tot_y = {}
+nb = len(listNameOfFiles(path))
+
+for nom in listNameOfFiles(path):
+    # Nom du fichier à importer
+    fich = open(path + '/' + nom, "r")
+    test_str = list(fich)[14:]
+    fich.close()
+    x = []
+    y = []
+    # Nettoyer les informations
+    for j in test_str:
+        elem_str = j.replace(",", ".").replace("\n", "").replace("\t", ",")
+        elem = elem_str.split(",")
+        x.append(float(elem[0]))
+        y.append(float(elem[1]))
+
+    donnees_tot_x = x
+    donnees_tot_y[nom] = y
+
+print(donnees_tot_y)
