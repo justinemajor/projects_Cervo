7d5134d2571cf0a8d75c9dd219138a81f8985bde
Justine Major <jumaj7@ulaval.ca>
2021-05-31 15:40:11 -0400

    travail pca
    j'essaie de comprendre les différentes étapes afin de les adapter ou mieux utiliser

--- PCA.py
+++ PCA.py
@@ -5,6 +5,7 @@ import os
 import fnmatch
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import numpy as np
 
 def listNameOfFiles(directory: str, extension="txt") -> list:
     foundFiles = []
@@ -40,10 +41,56 @@ for nom in listNameOfFiles(path):
     ordo.append(y)
     donnees_tot_y[nom] = y
 
+"""
 ordo = StandardScaler().fit_transform(ordo)
 pca = PCA(n_components=4)
 principalComponents = pca.fit_transform(ordo)
 principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2', 'principal component 3', 'principal component 4'])
 print(principalDf)
+print(StandardScaler().fit_transform(principalDf))
 print(listNameOfFiles(path).index("0100_10.txt"))
 print(listNameOfFiles(path)[4:6])
+"""
+
+
+def PCA(X , num_components):
+     
+    #Step-1
+    X_meaned = X - np.mean(X , axis = 0)
+     
+    #Step-2
+    cov_mat = np.cov(X_meaned , rowvar = False)
+     
+    #Step-3
+    eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)
+     
+    #Step-4
+    sorted_index = np.argsort(eigen_values)[::-1]
+    sorted_eigenvalue = eigen_values[sorted_index]
+    sorted_eigenvectors = eigen_vectors[:,sorted_index]
+     
+    #Step-5
+    eigenvector_subset = sorted_eigenvectors[:,0:num_components]
+     
+    #Step-6
+    X_reduced = np.dot(eigenvector_subset.transpose() , X_meaned.transpose() ).transpose()
+    X_reduced
+     
+    return X_reduced
+
+"""
+print(PCA(ordo, 4))
+print(listNameOfFiles(path).index("0001_10.txt"))
+print(listNameOfFiles(path).index("0010_10.txt"))
+print(listNameOfFiles(path).index("0100_10.txt"))
+print(listNameOfFiles(path).index("1000_10.txt"))
+print(PCA(ordo, 4)[7])
+"""
+
+ens = PCA(ordo, 4)
+
+print(ens)
+print(ens[0])
+print(ens[1])
+print(ens[3])
+print(ens[7])
---
+++ pca_rigged.py
@@ -0,0 +1,39 @@
+import csv
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import numpy as np
+
+A = np.array([1, 1, 1, 3, 1])
+B = np.array([1, 1, 1, 1, 5])
+C = np.array([1, 1, 3, 1, 1])
+D = np.array([2, 1, 1, 1, 1])
+
+X = np.array([A, B, C, D, 3*A+B, D+B, 5*C+A, C+D+B, 3*A+D+2*B])
+
+def PCA(X , num_components):
+     
+    #Step-1
+    X_meaned = X - np.mean(X , axis = 0)
+     
+    #Step-2
+    cov_mat = np.cov(X_meaned , rowvar = False)
+     
+    #Step-3
+    eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)
+     
+    #Step-4
+    sorted_index = np.argsort(eigen_values)[::-1]
+    sorted_eigenvalue = eigen_values[sorted_index]
+    sorted_eigenvectors = eigen_vectors[:,sorted_index]
+     
+    #Step-5
+    eigenvector_subset = sorted_eigenvectors[:,0:num_components]
+    
+    #Step-6
+    X_reduced = np.dot(eigenvector_subset.transpose() , X_meaned.transpose() ).transpose()
+    X_reduced
+     
+    return X_reduced
+
+print(PCA(X, 4))
