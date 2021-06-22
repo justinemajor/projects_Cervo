22e45270440aa54eac057afd9a8df035f1542cfa
Justine Major <jumaj7@ulaval.ca>
2021-06-22 10:22:23 -0400

    beginning of pca_coef

--- essais.py
+++ essais.py
@@ -3,10 +3,23 @@ import scipy as sp
 import numpy as np
 import matplotlib.pyplot as mpl
 import math
+import time
 
+"""
 a = [[1, 2, 3]]
 b = [[4, 5, 6]]
 
 c = a+b
 c = np.array(c)
 print(len(c))
+"""
+def isYouPretty():
+    return True
+
+def move_stage():
+    print("yay")
+
+print("joie")
+
+dico = {'yo':[1, 2, 3]}
+print(dico.keys())
