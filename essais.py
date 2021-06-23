5d550018e31b45c6d41b7e18bce5f0be698c9dcc
Justine Major <jumaj7@ulaval.ca>
2021-06-23 15:33:42 -0400

    tests for sweep with alternate direction in Optics-ID

--- essais.py
+++ essais.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as mpl
 import math
 import time
 
-
+"""
 a = [[1, 2, 3]]
 b = [[4, 5, 6]]
 
@@ -15,11 +15,11 @@ d = np.linalg.pinv(c)
 print(d)
 e = c@d
 print(e)
-"""
+
 def isYouPretty():
     return True
 
-def move_stage():
+def :
     print("yay")
 
 print("joie")
@@ -27,3 +27,39 @@ print("joie")
 dico = {'yo':[1, 2, 3]}
 print(dico.keys())
 """
+
+height = 3
+width = 3
+countWidth = 0
+countHeight = 0
+countSpectrums = 0
+triste = True
+while triste:
+    #print(f"({countWidth}, {countHeight})")
+    if countSpectrums < width*height:
+        if countWidth < width - 1 and countHeight % 2 == 0:
+            countWidth += 1
+            print(f"({countWidth}, {countHeight})")
+        elif countWidth == width - 1 and countHeight % 2 == 0:
+            countHeight += 1
+            if countHeight == height:
+                triste = False
+            else:
+                print(f"({countWidth}, {countHeight})")
+        elif 0 < countWidth < width and countHeight % 2 == 1:
+            countWidth -= 1
+            print(f"({countWidth}, {countHeight})")
+        elif countWidth == 0 and countHeight % 2 == 1:
+            countHeight += 1
+            if countHeight == height:
+                triste = False
+            else:
+                print(f"({countWidth}, {countHeight})")
+
+        else:
+            raise Exception("What the hell is going on?")
+
+        countSpectrums += 1
+
+    else:
+        triste = False
